66edb53483d73b88815ab1507c0dff8c587cffa7
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-08-18 22:33:21 +0300

    Display Checkins list

--- src/containers/CheckIns.js
+++ src/containers/CheckIns.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { ListItem } from "react-native-elements";
+import { connect } from "react-redux";
+import Constants from "expo-constants";
 
-import TitoCheckInApi from '../services/TitoCheckInApi'
+import TitoCheckInApi from "../services/TitoCheckInApi";
 
-export default class CheckIns extends Component {
-  static navigationOptions = {
-    title: 'CheckIns',
-  };
-
+class CheckIns extends Component {
   state = {
     isLoading: true,
     checkIns: [],
@@ -38,20 +34,58 @@ export default class CheckIns extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {
-          this.state.isLoading ?
-            <Text>Waiting for data</Text>
-            :
-            this.state.error === null ?
-              <>
-                <Text>Id: {this.state.checkIns.id}</Text>
-                <Text>Ticket Id: {this.state.checkInList.ticket_id}</Text>
-              </>
-              :
-              <Text>{this.state.error}</Text>
-        }
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View style={styles.statusBar} />
+
+        {this.state.isLoading ? (
+          <Text>Waiting for data</Text>
+        ) : this.state.error === null ? (
+          this._renderlist(this.state.checkIns)
+        ) : (
+          <Text>{this.state.error}</Text>
+        )}
       </View>
     );
   }
+
+  _renderlist = list => {
+    if (!list.length) {
+      return <Text>No checkins yet</Text>;
+    }
+
+    return (
+      <ScrollView style={{ flex: 1, width: "100%" }}>
+        {list.map(checkin => (
+          <ListItem
+            key={checkin.id}
+            title={`Fullname should be here ${checkin.id}`}
+            subtitle={`Ticket code should go here ${checkin.ticket_id}`}
+            topDivider
+            subtitleStyle={{ color: "#888888" }}
+          />
+        ))}
+      </ScrollView>
+    );
+  };
 }
+
+const styles = StyleSheet.create({
+  statusBar: {
+    height: Constants.statusBarHeight
+  }
+});
+
+const mapStateToProps = () => {
+  return state => {
+    return {
+      accountSettings: state.accountSettings
+    };
+  };
+};
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckIns);
