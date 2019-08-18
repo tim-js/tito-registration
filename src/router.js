f1c189609ef107e9ca55b74835a594e7e5b75832
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-08-18 22:59:20 +0300

    Add icons to Tabs

--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   createSwitchNavigator,
   createStackNavigator,
@@ -9,6 +10,8 @@ import Scan from "./containers/Scan";
 import CheckIns from "./containers/CheckIns";
 import * as StatusBar from "react-native";
 import Platform from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
 
 const headerStyle = {
   marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
@@ -25,9 +28,45 @@ export const SignedOut = createStackNavigator({
 
 export const SignedIn = createBottomTabNavigator(
   {
-    Dashboard,
-    Scan,
-    CheckIns
+    Dashboard: {
+      screen: Dashboard,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons
+            name="md-home"
+            size={20}
+            color={tintColor}
+            style={{ marginTop: 6 }}
+          />
+        )
+      }
+    },
+    Scan: {
+      screen: Scan,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons
+            name="md-qr-scanner"
+            size={20}
+            color={tintColor}
+            style={{ marginTop: 6 }}
+          />
+        )
+      }
+    },
+    CheckIns: {
+      screen: CheckIns,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons
+            name="md-checkmark-circle-outline"
+            size={20}
+            color={tintColor}
+            style={{ marginTop: 6 }}
+          />
+        )
+      }
+    }
   },
   {
     tabBarOptions: {
